0db549dc8e438ea7fb5a70167fdf74f4b4bdacfc
YMarroquin.multilab <yurimarroquin.multilab@gmail.com>
2022-06-09 17:55:57 -0500

    "agregar logica de calendario calenadario"

---
+++ application/controllers/TareaControlador.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TareaControlador extends UTP_Controller {
+
+    function __construct() {
+		parent::__construct();
+        date_default_timezone_set('America/lima');
+	  }
+
+
+}
